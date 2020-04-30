af1442b2aa6ec845592cb81a635f4824ccb5fa0d
Ryuma Nakano <ryuma.nakano@bambooanalytics.com.co>
2020-04-30 17:07:46 -0500

    Usar procesos asíncronos

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -11,7 +11,15 @@ export class ClasesComponent implements OnInit {
   propiedades: Object = {
     danger: false,
   };
+
+  loading: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  ejecutar() {
+    this.loading = true;
+
+    setTimeout(() => (this.loading = false), 3000);
+  }
 }
