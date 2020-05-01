3eaf91f758b1f34f8426f511b99cfe496badaf58
Ryuma Nakano <ryuma.nakano@bambooanalytics.com.co>
2020-05-01 11:52:28 -0500

    Crear la ruta de los usuarios y sus componenetes para poder editar, ver detalle, etc

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClasesComponent implements OnInit {
   alerta: string = 'alert-success';
 
-  propiedades: Object = {
+  propiedades: any = {
     danger: false,
   };
 
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <app-ng-style></app-ng-style>
+    <app-css></app-css>
+    <app-clases></app-clases>
+    <p [appResaltado]="'lightgray'">
+      Hola mundo
+    </p>
+    <app-ng-switch></app-ng-switch>
+  `,
+  styles: [],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
