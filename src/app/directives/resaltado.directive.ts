e16eff14a63a5027ffd4169ec539b74b2853c3c6
Ryuma Nakano <ryuma.nakano@bambooanalytics.com.co>
2020-05-01 10:54:01 -0500

    Crear directivas para el manejo dinámico de presentación de los objetos

---
+++ src/app/directives/resaltado.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]',
+})
+export class ResaltadoDirective {
+  constructor(private el: ElementRef) {}
+
+  @Input('appResaltado') nuevoColor: string;
+
+  @HostListener('mouseenter') mouseEntro() {
+    this.resaltar(this.nuevoColor || 'yellow');
+  }
+  @HostListener('mouseleave') mouseSalio() {
+    this.el.nativeElement.style.backgroundColor = null;
+  }
+
+  private resaltar(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
